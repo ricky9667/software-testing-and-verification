a0fe531ad9d2801af9bb72d668681bb51c0cf293
胡紹宇 <t112598045@ntut.edu.tw>
2024-04-27 15:11:24 +0800

    test: 💍 add test cases for Geomem.createRegionFilter()

--- src/test/java/com/github/davidmoten/geo/mem/GeomemTest.java
+++ src/test/java/com/github/davidmoten/geo/mem/GeomemTest.java
@@ -1,10 +1,12 @@
 package com.github.davidmoten.geo.mem;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class GeomemTest {
 
@@ -38,5 +40,21 @@ public class GeomemTest {
             size++;
         }
         return size;
+    }
+
+    @Test
+    public void testCreateRegionFilterWithInfoInPredicate() {
+        Predicate<Info<String, String>> predicate = geomem.createRegionFilter(50.0, -70.0, 40.0, -50.0);
+        Info<String, String> info = new Info<String, String>(45.0, -60.0, 1708757706, "test", Optional.of("test"));
+
+        assertTrue(predicate.apply(info));
+    }
+
+    @Test
+    public void testCreateRegionFilterWithInfoNotInPredicate() {
+        Predicate<Info<String, String>> predicate = geomem.createRegionFilter(50.0, -70.0, 40.0, -50.0);
+        Info<String, String> info = new Info<String, String>(70.0, -60.0, 1708757706, "test", Optional.of("test"));
+
+        assertFalse(predicate.apply(info));
     }
 }
